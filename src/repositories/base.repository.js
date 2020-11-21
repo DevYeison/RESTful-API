f4a33455b105a50aab14b4cb141390ad9c4c3bbc
Yeison <toxiicsa@hotmail.com>
2020-11-20 21:12:40 -0500

    ADD: return true in delete method

--- src/repositories/base.repository.js
+++ src/repositories/base.repository.js
@@ -19,7 +19,8 @@ class BaseRepository {
     }
 
     async delete(id){
-        return await this.model.findByIdAndDelete(id);
+        await this.model.findByIdAndDelete(id);
+        return true; 
     }
 }
 
