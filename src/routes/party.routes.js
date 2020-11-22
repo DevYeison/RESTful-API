d20f1c7cb19d60571f00f355cdaee5513abe225e
Yeison <toxiicsa@hotmail.com>
2020-11-22 01:26:01 -0500

    FIX: change name in the route

--- src/routes/party.routes.js
+++ src/routes/party.routes.js
@@ -5,13 +5,13 @@ module.exports = function({PartyController}){
     const router = Router();
 
     router.get("/:partyId", PartyController.get);
-    router.get("", [ParseIntMiddleware],PartyController.getAll);
-    router.get("userId/all", PartyController.getUserParties);
+    router.get("", [ParseIntMiddleware], PartyController.getAll);
+    router.get("/:userId/all", PartyController.getUserParties);
     router.post("", PartyController.create);
     router.patch("/:partyId", [AuthMiddleware], PartyController.update);
     router.delete("/:partyId", [AuthMiddleware], PartyController.delete);
-    router.post(":partyId/upvote", [AuthMiddleware], PartyController.upvoteParty);
-    router.post(":partyId/downvote", [AuthMiddleware] ,PartyController.downvoteParty);
+    router.post("/:partyId/upvote", [AuthMiddleware], PartyController.upvoteParty);
+    router.post("/:partyId/downvote", [AuthMiddleware] ,PartyController.downvoteParty);
     
     return router;
 }
