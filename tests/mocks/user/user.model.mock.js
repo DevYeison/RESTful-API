49f6a724e00ac0044a33c3398d297e5ee798aa28
Yeison <toxiicsa@hotmail.com>
2020-11-21 20:43:21 -0500

    ADD: test suites

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+    user: {
+        "_id": "5fb8ba8b0c1c7a505c8ecf23",
+        "name": "Yeison",
+        "username": "Toxic",
+        "password": "123abc"
+    },
+    users:[
+        {
+            "_id": "5fb8d1836969245e34d02c9b",
+            "name": "Yeison",
+            "username": "Toxic1",
+            "password": "123abc"
+        },
+        {
+            "_id": "5fb8d1886969245e34d02c9c",
+            "name": "Yeison",
+            "username": "Toxic2",
+            "password": "123abc"
+        },
+    ]
+};
---
+++ tests/unit/services/user.service.test.js
@@ -0,0 +1,61 @@
+const { UserService } = require('../../../src/services');
+const { UserRepositoryMock } = require('../../mocks');
+let { 
+    UserModelMock: { user, users} 
+} = require('../../mocks');
+
+describe("User service test", ()=>{
+    beforeEach(()=>{
+        jest.clearAllMocks;
+    });
+
+    it("Should return a user by id", async () =>{
+        const UserRepository = UserRepositoryMock;
+        UserRepository.get.mockReturnValue(user);
+        
+        const _userService = new UserService({ UserRepository });
+        const expected = await _userService.get(user._id);
+
+        expect(expected).toMatchObject(user);
+    });
+
+    it("Should find a user by username", async () =>{
+        const UserRepository = UserRepositoryMock;
+        UserRepository.getUserByUsername.mockReturnValue(user);
+        
+        const _userService = new UserService({ UserRepository });
+        const expected = await _userService.getUserByUsername(user.username);
+
+        expect(expected).toMatchObject(user);
+    });
+
+    it("Should return a user collection", async ()=>{
+        const UserRepository = UserRepositoryMock;
+        UserRepository.getAll.mockReturnValue(users);
+        
+        const _userService = new UserService({ UserRepository });
+        const expected = await _userService.getAll();
+
+        expect(expected).toMatchObject(users);
+    });
+
+    it("Should update an especific user by id", async () =>{
+        const UserRepository = UserRepositoryMock;
+        UserRepository.update.mockReturnValue(user);
+        
+        const _userService = new UserService({ UserRepository });
+        const expected = await _userService.repository.update(user._id, user);
+
+        expect(expected).toMatchObject(user);
+    });
+
+    it("Should delete an especific user by id", async () =>{
+        const UserRepository = UserRepositoryMock;
+        UserRepository.delete.mockReturnValue(true);
+        
+        const _userService = new UserService({ UserRepository });
+        const expected = await _userService.repository.delete(user._id);
+
+        expect(expected).toEqual(true);
+    });
+});
