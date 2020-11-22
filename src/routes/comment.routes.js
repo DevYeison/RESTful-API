d9f5bbe479ef6fb5d8829bf004784ba9cdad38d9
Yeison <toxiicsa@hotmail.com>
2020-11-22 01:48:44 -0500

    ADD: auth in comments

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
+const { AuthMiddleware } = require('../middlewares');
 
 module.exports = function({CommentController}){
     const router = Router();
 
     router.get("/:commentId", CommentController.get);
     router.get("/:partyId/unique", CommentController.getPartyComments);
-    router.post("/:partyId", CommentController.createComment);
-    router.patch("/:commentId", CommentController.update);
-    router.delete("/:commentId", CommentController.delete);
+    router.post("/:partyId", AuthMiddleware, CommentController.createComment);
+    router.patch("/:commentId", AuthMiddleware, CommentController.update);
+    router.delete("/:commentId", AuthMiddleware, CommentController.delete);
     return router;
 }
